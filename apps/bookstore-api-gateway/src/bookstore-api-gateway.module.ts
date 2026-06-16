f7dc436533305da919ad295f4fe535fa40f3484f
caleb <nascal3@gmail.com>
2026-06-16 14:44:30 +0300

    convert books app to a microservice, update imports, and remove redundant boilerplate

--- apps/bookstore-api-gateway/src/bookstore-api-gateway.module.ts
+++ apps/bookstore-api-gateway/src/bookstore-api-gateway.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookstoreApiGatewayController } from './bookstore-api-gateway.controller';
 import { BookstoreApiGatewayService } from './bookstore-api-gateway.service';
 import { UsersModule } from './users/users.module';
+import { BooksModule } from '../../books/src/books-app.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BooksModule],
   controllers: [BookstoreApiGatewayController],
   providers: [BookstoreApiGatewayService],
 })
