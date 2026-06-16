e7248b642b5adfa4d4b140d2bc8e1707cc9dd8aa
caleb <nascal3@gmail.com>
2026-06-16 14:47:46 +0300

    remove BooksModule import from bookstore-api-gateway module

--- apps/bookstore-api-gateway/src/bookstore-api-gateway.module.ts
+++ apps/bookstore-api-gateway/src/bookstore-api-gateway.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { BookstoreApiGatewayController } from './bookstore-api-gateway.controller';
 import { BookstoreApiGatewayService } from './bookstore-api-gateway.service';
 import { UsersModule } from './users/users.module';
-import { BooksModule } from '../../books/src/books-app.module';
 
 @Module({
-  imports: [UsersModule, BooksModule],
+  imports: [UsersModule],
   controllers: [BookstoreApiGatewayController],
   providers: [BookstoreApiGatewayService],
 })
