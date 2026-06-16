685a0b34fe70af65057deb6835c3e695c00a59fc
caleb <nascal3@gmail.com>
2026-06-16 21:09:24 +0300

    create contracts library with shared DTOs and patterns

---
+++ libs/contracts/src/index.ts
@@ -0,0 +1,7 @@
+export * from '@app/contracts/users/user.dto';
+export * from '@app/contracts/books/book.dto';
+export * from '@app/contracts/books/create-book.dto';
+export * from '@app/contracts/books/update-book.dto';
+export * from '@app/contracts/users/users.patterns';
+export * from '@app/contracts/books/books.patterns';
+
